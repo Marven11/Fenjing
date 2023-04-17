ebea5f5cd8c1dc502660932d57bc47983b1904ec
Your Name <you@example.com>
2023-04-18 00:24:06 +0800

    Rename StrPatterns

--- fenjing/pattern.py
+++ fenjing/pattern.py
@@ -641,7 +641,7 @@ class StrPattern(BasePattern):
     pass
 
 
-class StrPattern1(StrPattern):
+class StrPattern01(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
         self.inner_s = inner_s.replace("'", "\\'")
@@ -652,7 +652,7 @@ class StrPattern1(StrPattern):
         return "'" + self.inner_s + "'"
 
 
-class StrPattern2(StrPattern):
+class StrPattern02(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
         self.inner_s = inner_s.replace('"', '\\"')
@@ -663,7 +663,7 @@ class StrPattern2(StrPattern):
         return '"' + self.inner_s + '"'
 
 
-class StrPattern3(StrPattern):
+class StrPattern03(StrPattern):
     def __init__(self, inner_s):
         from urllib.parse import quote
         super().__init__()
@@ -675,7 +675,7 @@ class StrPattern3(StrPattern):
         return '"' + self.inner_s + '"'
 
 
-class StrPattern4(StrPattern):
+class StrPattern04(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
         self.require(PlainPattern, "'")
@@ -689,7 +689,7 @@ class StrPattern4(StrPattern):
         return "(%s)" % "+".join(l)
 
 
-class StrPattern5(StrPattern):
+class StrPattern05(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
         self.require(PlainPattern, "\"")
@@ -703,7 +703,7 @@ class StrPattern5(StrPattern):
         return "(%s)" % "+".join(l)
 
 
-class StrPattern6(StrPattern):
+class StrPattern06(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
         self.pattern = "('%c'*{})%({})"
@@ -720,7 +720,7 @@ class StrPattern6(StrPattern):
         )
 
 
-class StrPattern7(StrPattern):
+class StrPattern07(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
         self.pattern = "(\"%c\"*{})%({})"
@@ -737,7 +737,7 @@ class StrPattern7(StrPattern):
         )
 
 
-class StrPattern8(StrPattern):
+class StrPattern08(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
         import re
@@ -758,7 +758,7 @@ class StrPattern8(StrPattern):
             self.use(IntPattern, 1)
         )
 
-class StrPattern9(StrPattern):
+class StrPattern09(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
         import re
@@ -827,7 +827,7 @@ class StrPattern12(StrPattern):
     def _generate(self):
         return "'" + self.inner_s + "'"
 
-class StrPattern101(StrPattern):
+class StrPattern13(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
 
@@ -861,7 +861,7 @@ class StrPattern101(StrPattern):
             numbers
         )
 
-class StrPattern102(StrPattern):
+class StrPattern14(StrPattern):
     def __init__(self, inner_s):
         super().__init__()
 
