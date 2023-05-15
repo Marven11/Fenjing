e37a568f034552ba4e96b5af651fb9bf09326fd6
Marven11 <Marven11@example.com>
2023-05-15 12:00:57 +0800

    Add config payload & fix full_payload_store

--- fenjing/config_payload.py
+++ fenjing/config_payload.py
@@ -1,18 +1,17 @@
 from typing import Callable, Tuple, Dict
-from .const import OS_POPEN_READ
+from .const import CONFIG
 from .full_payload_gen import FullPayloadGen
 
 full_payload_store: Dict[int, FullPayloadGen] = {}
 
-def exec_cmd_payload(waf_func: Callable[[str, ], bool], cmd: str) -> Tuple[str | None, bool | None]:
-    """根据提供的waf函数为一个shell命令生成对应的payload
+def exec_cmd_payload(waf_func: Callable[[str, ], bool]) -> str | None:
+    """根据提供的waf函数生成读取config的payload
 
     Args:
         waf_func (Callable[[str, ], bool]): waf函数，判断提供的payload能否通过waf, 能则返回True
-        cmd (str): 需要执行的shell命令
 
     Returns:
-        Tuple[str|None, bool|None]: 对应的payload, 以及payload是否能生成回显
+        str|None: payload
     """
     full_payload = None
     if id(waf_func) not in full_payload_store:
@@ -20,5 +19,8 @@ def exec_cmd_payload(waf_func: Callable[[str, ], bool], cmd: str) -> Tuple[str |
         full_payload_store[id(waf_func)] = full_payload
     else:
         full_payload = full_payload_store[id(waf_func)]
-    return full_payload.generate(OS_POPEN_READ, cmd)
+    payload, will_print = full_payload.generate(CONFIG)
+    if not will_print:
+        return None
+    return payload
 
