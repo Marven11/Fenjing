6775f30cf7dac6a4b04892c064f504e16ebd9467
Marven11 <110723864+Marven11@users.noreply.github.com>
2023-05-30 23:18:29 +0800

    Import const and FullPayloadGen in __init__.py

--- fenjing/__init__.py
+++ fenjing/__init__.py
@@ -1,4 +1,5 @@
-from . import exceptions, payload_gen
+from . import exceptions, payload_gen, const
+from .full_payload_gen import FullPayloadGen
 from .shell_payload import exec_cmd_payload
 from .config_payload import config_payload
 from .int_vars import get_useable_int_vars
