f8b9ec6d71707ab04e6f57035834a569aeee2933
Marven11 <110723864+Marven11@users.noreply.github.com>
2023-06-03 00:50:54 +0800

    Bug fix: removed exceptions

--- fenjing/__init__.py
+++ fenjing/__init__.py
@@ -1,4 +1,4 @@
-from . import exceptions, payload_gen, const
+from . import payload_gen, const
 from .full_payload_gen import FullPayloadGen
 from .shell_payload import exec_cmd_payload
 from .config_payload import config_payload
