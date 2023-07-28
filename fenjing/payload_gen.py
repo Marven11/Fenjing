ecffe7c9499222f141c0fdbd0e12db596d26a034
Marven11 <110723864+Marven11@users.noreply.github.com>
2023-07-28 15:27:45 +0800

    Update fast mode weights

--- fenjing/payload_gen.py
+++ fenjing/payload_gen.py
@@ -23,8 +23,8 @@ gen_weight_default = {
     "gen_string_percent_lower_c_concat": 1,
     "gen_string_lower_c_joinerbatch": 1,
     "gen_string_percent_urlencode2": 1,
-    "gen_string_x1": 1,
-    "gen_string_x2": 1,
+    "gen_string_concat1": 1,
+    "gen_string_concat2": 1,
     "gen_string_formatpercent": 1,
     "gen_attribute_attrfilter": 1,
     "gen_item_dunderfunc": 1
