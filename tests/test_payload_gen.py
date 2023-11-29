a6e4465d0c2f4dd35f0f5205293bb284fa6490bf
Marven11 <110723864+Marven11@users.noreply.github.com>
2023-11-30 00:27:59 +0800

    Add tests

--- tests/test_payload_gen.py
+++ tests/test_payload_gen.py
@@ -6,9 +6,11 @@ import unittest
 import fenjing
 
 
-from fenjing.payload_gen import PayloadGenerator
+from fenjing.payload_gen import PayloadGenerator, expression_gens
 from fenjing import const
 import logging
+
+from jinja2 import Template, TemplateError
 
 fenjing.payload_gen.logger.setLevel(logging.ERROR)
 
@@ -146,3 +148,43 @@ class PayloadGenTestCaseHard(unittest.TestCase):
         self.assertIsNotNone(
             self.payload_gen.generate(const.OS_POPEN_READ, "echo fen  jing;")
         )
+
+class PayloadGenTestsTargetRules(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.context = {
+            "l": 1,
+            "e": 3,
+            "lo": 10,
+            "loo": 100,
+            "eoo": 300,
+        }
+        self.context_payload = "{%set l=1%}{%set e=3%}{%set lo=10%}{%set loo=100%}{%set eoo=300%}"
+        self.payload_gen = get_payload_gen([],self.context)
+
+    def target_test(self, target):
+        gen_type = target[0]
+        for gen_func in expression_gens[gen_type]:
+            try:
+                target_list = gen_func(self.context, *target[1:])
+            except Exception as e:
+                raise RuntimeError(f"Generate failed for rule {gen_func.__name__}") from e
+            result = self.payload_gen.generate_by_list(target_list)
+
+            if not result:
+                continue
+            try:
+                str_result, _, _ = result
+                Template(self.context_payload + f"Hello, {str_result}").render()
+            except TemplateError as e:
+                raise RuntimeError(f"Render failed for rule {gen_func.__name__}") from e
+
+    def test_targets(self):
+        targets = [
+            (const.INTEGER, 123),
+            (const.STRING, "echo 'cracked\"\\'"),
+            (const.OS_POPEN_READ, "echo 'cracked\"\\'"),
+        ]
+        for target in targets:
+            self.target_test(target)
