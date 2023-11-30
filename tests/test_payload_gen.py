41222e6b616bc5da4f1649123d041d2087f49e2e
Marven11 <110723864+Marven11@users.noreply.github.com>
2023-11-30 14:03:29 +0800

    Add tests

--- tests/test_payload_gen.py
+++ tests/test_payload_gen.py
@@ -22,6 +22,55 @@ def get_payload_gen(blacklist, context):
     return PayloadGenerator(waf_func, context)
 
 
+
+class PayloadGenTestsTargetRules(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.context = {
+            "l": 1,
+            "e": 3,
+            "lo": 10,
+            "loo": 100,
+            "eoo": 300,
+        }
+        self.context_payload = "{%set l=1%}{%set e=3%}{%set lo=10%}{%set loo=100%}{%set eoo=300%}"
+        self.payload_gen = get_payload_gen([],self.context)
+
+    def target_test(self, target):
+        gen_type = target[0]
+        for gen_func in expression_gens[gen_type]:
+            try:
+                target_list = gen_func(self.context, *target[1:])
+            except Exception as e:
+                raise RuntimeError(f"Generate failed for rule {gen_func.__name__}") from e
+            result = self.payload_gen.generate_by_list(target_list)
+
+            if not result:
+                continue
+            try:
+                str_result, _, _ = result
+                Template(self.context_payload + f"Hello, {str_result}").render()
+            except TemplateError as e:
+                raise RuntimeError(f"Render failed for rule {gen_func.__name__}") from e
+
+    def test_targets(self):
+        targets = [
+            (const.INTEGER, 123),
+            (const.PLUS, (const.INTEGER, 98), (const.INTEGER, 37)),
+            (const.MULTIPLY, (const.INTEGER, 3), (const.INTEGER, 37)),
+            (const.STRING_PERCENT,),
+            (const.STRING_LOWERC,),
+            (const.STRING_MANY_PERCENT_LOWER_C, 4),
+            (const.STRING, "__class__"),
+            (const.STRING, "echo 'cracked\"\\'"),
+            (const.OS_POPEN_READ, "echo 'cracked\"\\'"),
+        ]
+        for target in targets:
+            logging.info("Testing target: %s", repr(target))
+            self.target_test(target)
+
+
 class PayloadGenTestCaseSimple(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
@@ -134,6 +183,22 @@ class PayloadGenTestCaseHard(unittest.TestCase):
             },
         )
 
+    def test_targets(self):
+        targets = [
+            (const.STRING_PERCENT, ),
+            (const.STRING_LOWERC, ),
+            (const.INTEGER, 10),
+            (const.STRING, "a"),
+            (const.STRING_CONCAT, (const.STRING, "a"), (const.STRING, "a")),
+            (const.STRING_PERCENT_LOWER_C, ),
+            (const.STRING_MANY_PERCENT_LOWER_C, 10),
+            (const.STRING, "ls /;"),
+
+        ]
+        for target in targets:
+            self.assertIsNotNone(self.payload_gen.generate(target[0], *target[1:]), repr(target))
+
+
     def test_string(self):
         strings = [
             "123",
@@ -142,49 +207,9 @@ class PayloadGenTestCaseHard(unittest.TestCase):
             "__import__('os').popen('echo test_command/$(ls / | base64 -w)').read()",
         ]
         for string in strings:
-            self.assertIsNotNone(self.payload_gen.generate(const.STRING, string))
+            self.assertIsNotNone(self.payload_gen.generate(const.STRING, string), string)
 
     def test_os_popen_read(self):
         self.assertIsNotNone(
             self.payload_gen.generate(const.OS_POPEN_READ, "echo fen  jing;")
         )
-
-class PayloadGenTestsTargetRules(unittest.TestCase):
-
-    def setUp(self):
-        super().setUp()
-        self.context = {
-            "l": 1,
-            "e": 3,
-            "lo": 10,
-            "loo": 100,
-            "eoo": 300,
-        }
-        self.context_payload = "{%set l=1%}{%set e=3%}{%set lo=10%}{%set loo=100%}{%set eoo=300%}"
-        self.payload_gen = get_payload_gen([],self.context)
-
-    def target_test(self, target):
-        gen_type = target[0]
-        for gen_func in expression_gens[gen_type]:
-            try:
-                target_list = gen_func(self.context, *target[1:])
-            except Exception as e:
-                raise RuntimeError(f"Generate failed for rule {gen_func.__name__}") from e
-            result = self.payload_gen.generate_by_list(target_list)
-
-            if not result:
-                continue
-            try:
-                str_result, _, _ = result
-                Template(self.context_payload + f"Hello, {str_result}").render()
-            except TemplateError as e:
-                raise RuntimeError(f"Render failed for rule {gen_func.__name__}") from e
-
-    def test_targets(self):
-        targets = [
-            (const.INTEGER, 123),
-            (const.STRING, "echo 'cracked\"\\'"),
-            (const.OS_POPEN_READ, "echo 'cracked\"\\'"),
-        ]
-        for target in targets:
-            self.target_test(target)
