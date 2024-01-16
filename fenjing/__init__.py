b8529fd7df5ce459ee302b71ee95e3da373255da
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-01-16 13:32:09 +0800

    Export Options class

--- fenjing/__init__.py
+++ fenjing/__init__.py
@@ -3,3 +3,4 @@ from .full_payload_gen import FullPayloadGen
 from .shell_payload import exec_cmd_payload
 from .config_payload import config_payload
 from .form import get_form, fill_form
+from .options import Options
