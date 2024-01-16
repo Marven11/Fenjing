7cdedab67ab54967b304e735a3c59807726fdc93
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-01-16 18:19:33 +0800

    Improve run_local.sh

--- tests/run_local.sh
+++ tests/run_local.sh
@@ -2,5 +2,5 @@ export VULUNSERVER_ADDR="http://127.0.0.1:5000"
 export SLEEP_INTERVAL=0.01
 python vulunserver.py 2>/dev/null &
 vulserver_pid=$!
+trap "trap - SIGTERM && kill $vulserver_pid" SIGINT SIGTERM EXIT
 python -m unittest *.py
-kill $vulserver_pid
