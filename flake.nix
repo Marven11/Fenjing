254b2f89b1c5859f4a539abece75835f6cab9bfc
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-05-31 22:37:42 +0800

    Configure python37 environment

--- flake.nix
+++ flake.nix
@@ -3,44 +3,52 @@
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
+    nixpkgs_old.url = "https://github.com/NixOS/nixpkgs/archive/b4e193a23a1c5d8794794e65cabf1f1135d07fd9.tar.gz";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs_old,
     utils,
     ...
   }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+      pkgs_old = import nixpkgs_old {inherit system;};
       pythonPackages = pkgs.python3Packages;
+      python37Packages = pkgs_old.python37Packages;
     in {
       devShells.default = pkgs.mkShell {
         name = "python-venv";
         venvDir = "./.venv";
         buildInputs = [
-          # A Python interpreter including the 'venv' module is required to bootstrap
-          # the environment.
           pythonPackages.python
-
-          # This executes some shell code to initialize a venv in $venvDir before
-          # dropping into the shell
           pythonPackages.venvShellHook
-
-          # Those are dependencies that we would like to use from nixpkgs, which will
-          # add them to PYTHONPATH and thus make them accessible from within the venv.
           pythonPackages.notebook
           pythonPackages.ipython
         ];
-
-        # Run this command, only after creating the virtual environment
         postVenvCreation = ''
           unset SOURCE_DATE_EPOCH
           pip install -r requirements-devel.txt
         '';
-
-        # Now we can execute any commands within the virtual environment.
-        # This is optional and can be left out to run pip manually.
+        postShellHook = ''
+          # allow pip to install wheels
+          unset SOURCE_DATE_EPOCH
+        '';
+      };
+      # python3.7 is supported because some people are still using it.
+      devShells.python37 = pkgs_old.mkShell {
+        name = "python-venv";
+        venvDir = "./.venv";
+        buildInputs = [
+          python37Packages.python
+          python37Packages.venvShellHook
+        ];
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install -r requirements-devel-python37.lock
+        '';
         postShellHook = ''
           # allow pip to install wheels
           unset SOURCE_DATE_EPOCH
