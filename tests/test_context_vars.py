8d67d955bc48996b16e7b2cfb008fd14e50abd33
Marven11 <110723864+Marven11@users.noreply.github.com>
2025-01-16 08:10:35 +0800

    Add tests

---
+++ tests/test_context_vars.py
@@ -0,0 +1,37 @@
+import sys  # noqa
+
+sys.path.append("..")  # noqa
+
+import unittest
+import logging
+import os
+
+import requests
+
+import fenjing
+from fenjing.context_vars import const_exprs, const_exprs_py3, const_exprs_flask
+
+from jinja2 import Template
+
+VULUNSERVER_ADDR = os.environ.get("VULUNSERVER_ADDR", "http://127.0.0.1:5000")
+
+fenjing.payload_gen.logger.setLevel(logging.ERROR)
+
+
+class ContextVarsTests(unittest.TestCase):
+    def test_const_exprs(self):
+        exprs = {**const_exprs, **const_exprs_py3}
+        for k, v in exprs.items():
+            payload = "{%if (EXPR)==(VALUE)%}yes{%endif%}".replace("EXPR", k).replace(
+                "VALUE", repr(v)
+            )
+            result = Template(payload).render()
+            assert "yes" in result, f"Test Failed for {k!r}"
+
+    def test_const_exprs_flask(self):
+        for k, v in const_exprs_flask.items():
+            payload = "{%if (EXPR)==(VALUE)%}yes{%endif%}".replace("EXPR", k).replace(
+                "VALUE", repr(v)
+            )
+            r = requests.get(VULUNSERVER_ADDR, params={"name": payload}, timeout=1)
+            assert "yes" in r.text, f"Test Failed for {k!r}"
